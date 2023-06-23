2aef266b9199f2a2f4372b54831cdb30ec576699
SzymonB5 <235831@edu.p.lodz.pl>
2023-06-23 13:49:27 +0200

    Initial commit

---
+++ src/main/java/com/example/PKiAMDataEntry/PKiAmDataEntryApplication.java
@@ -0,0 +1,13 @@
+package com.example.PKiAMDataEntry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PKiAmDataEntryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PKiAmDataEntryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PKiAMDataEntry/PKiAmDataEntryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PKiAMDataEntry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PKiAmDataEntryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
