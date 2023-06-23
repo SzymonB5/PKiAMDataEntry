dbb3a1dc8dcc5f3d61502adc7c95bf01e0f16b69
SzymonB5 <235831@edu.p.lodz.pl>
2023-06-23 14:03:20 +0200

    Prepare frontend

--- src/main/java/com/example/PKiAMDataEntry/PKiAmDataEntryApplication.java
+++ src/main/java/com/example/PKiAMDataEntry/PKiAmDataEntryApplication.java
@@ -18,4 +18,31 @@ public class PKiAmDataEntryApplication {
 		return "Test 123";
 	}
 
+	@GetMapping("/addUser")
+	public String addUser() {
+		StringBuilder stringBuilder = new StringBuilder("<html>");
+		stringBuilder.append("<body>");
+
+		stringBuilder.append("<label> FirstName </label><br/>");
+		stringBuilder.append("<input type=\"text\" id=\"tbFirstName\"/><br/>");
+
+		stringBuilder.append("<label> LastName </label><br/>");
+		stringBuilder.append("<input type=\"text\" id=\"tbLastName\"/><br/>");
+
+		stringBuilder.append("<label> Password </label><br/>");
+		stringBuilder.append("<input type=\"text\" id=\"tbPassword\"/><br/>");
+
+		stringBuilder.append("<button type=\"button\" onclick=\"myFunction()\">Submit</button><br/>");
+
+		stringBuilder.append("<script>");
+		stringBuilder.append("function myFunction() {");
+		stringBuilder.append("    alert(\"Button clicked!\");");
+		stringBuilder.append("}");
+		stringBuilder.append("</script>");
+
+		stringBuilder.append("</body>");
+		stringBuilder.append("</html>");
+		return stringBuilder.toString();
+	}
+
 }
